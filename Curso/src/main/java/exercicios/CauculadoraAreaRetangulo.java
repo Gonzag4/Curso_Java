152829440317dd386e434d769b010e64eb884993
LuizGonzaga931 <150976494+Gonzag4@users.noreply.github.com>
2025-09-19 00:29:45 -0300

    atualização dos exercícios

---
+++ Curso/src/main/java/exercicios/CauculadoraAreaRetangulo.java
@@ -0,0 +1,14 @@
+package exercicios;
+
+public class CauculadoraAreaRetangulo {
+    public static void main(String[] args){
+        double largura = 5.0;
+        double altura = 3.0;
+
+        double area = largura * altura;
+
+        System.out.println("A largura do triangulo retangulo: " + largura);
+        System.out.println("A altura do triangulo retangulo: " + altura);
+        System.out.println("A area do triangulo retangulo: " + area);
+    }
+}
--- Curso/src/main/java/exercicios/ControleDeEstoqueEmUmaPadaria.java
+++ Curso/src/main/java/exercicios/ControleDeEstoqueEmUmaPadaria.java
@@ -1,4 +1,20 @@
 package exercicios;
 
 public class ControleDeEstoqueEmUmaPadaria {
+    public static void main(String[] args){
+        int quantidadeFarinha = 10;
+        int quantidadeDeAcucar = 5;
+
+        System.out.println("A quantidade de açucar é: " + quantidadeDeAcucar);
+
+        System.out.println(" A quantidade de Farinha é: " + quantidadeFarinha);
+
+    }
 }
+/*
+ Explicação do Código:
+Criamos a classe ControleEstoquePadaria .
+Declaramos as variáveis quantidadeFarinha e quantidadeAcucar .
+Atribuímos valores iniciais a essas variáveis.
+As mensagens são exibidas na tela, mostrando as quantidades de farinha e açúcar disponíveis.
+*/
