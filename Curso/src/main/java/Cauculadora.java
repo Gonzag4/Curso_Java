9d094946bde4c6c531ccf3bfb4603af58cf4e806
Gonzag4 <gonzagaluiz931@outlook.com>
2025-09-26 18:22:06 -0300

    Cauculadira

---
+++ Curso/src/main/java/Cauculadora.java
@@ -0,0 +1,18 @@
+public class Cauculadora {
+    final int soma ( int a, int b){
+        return a + b;
+    }
+
+
+
+}
+class CalculadoraCientifica extends Cauculadora{
+
+    // Tente descomentar o trecho abaixo para ver o erro:
+    // @Override
+    // int somar(int a, int b) {
+    // return a + b;
+    // }
+
+}
+
