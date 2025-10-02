c298758e3769deecd36c36bb12fc06647fe72214
Gonzag4 <gonzagaluiz931@outlook.com>
2025-10-02 14:18:49 -0300

    atualização

--- Curso/src/main/java/ExercicioInterface/PedidoRestaurante.java
+++ Curso/src/main/java/ExercicioInterface/PedidoRestaurante.java
@@ -1,4 +1,32 @@
 package ExercicioInterface;
 
-public interface PedidoRestaurante {
+/*public interface PedidoRestaurante {
+    void adicionarItem(String item, double preco);
+    double calcularValorTotal();
 }
+
+public class Pedido implements PedidoRestaurante{
+    private double total = 0;
+
+    @Override
+    public void adicionarItem(String item, double preco){
+        System.out.println("Adicionando " + item + " (R$" + preco + ") ao pedido.");
+        total +=preco;
+    }
+
+    @Override
+    public double calcularTotal(){
+        return total;
+    }
+
+    public static void main(String[] args){
+        PedidoRestaurante pedido = new Pedido();
+        pedido.adicionarItem("Hamburguer," 15.0);
+        pedido.adicionarItem("Batata Frita", 8.0);
+        pedido.adicionarItem("Refrigerante", 4.0);
+
+        System.out.println("Valor total do pedido: R$" + pedido.calcularValorTotal());
+    }
+}
+
+*/
